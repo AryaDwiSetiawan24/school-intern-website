06f560728e1b8a27bed0ed9f9134070b3f1ddf61
Arya Dwi S <aryadwis24@gmail.com>
2025-03-10 08:57:00 +0700

    first commit

---
+++ routes/admin.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataFeedController;
+use App\Http\Controllers\DashboardController;
+
+// Halaman admin
+Route::redirect('/', 'login');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    // Route for the getting the data feed
+    Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/analytics', [DashboardController::class, 'analytics'])->name('analytics');
+    Route::get('/dashboard/fintech', [DashboardController::class, 'fintech'])->name('fintech');
+
+    Route::get('/settings/account', function () {
+        return view('pages/settings/account');
+    })->name('account');
+    Route::get('/settings/notifications', function () {
+        return view('pages/settings/notifications');
+    })->name('notifications');
+    Route::get('/settings/apps', function () {
+        return view('pages/settings/apps');
+    })->name('apps');
+    Route::get('/settings/plans', function () {
+        return view('pages/settings/plans');
+    })->name('plans');
+    Route::get('/settings/billing', function () {
+        return view('pages/settings/billing');
+    })->name('billing');
+    Route::get('/settings/feedback', function () {
+        return view('pages/settings/feedback');
+    })->name('feedback');
+});
