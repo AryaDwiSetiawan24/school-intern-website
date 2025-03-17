06a6b0c926aedca9442671cc67cba0e9f7a94e62
alhalimthoriqfebrian <alhalimthof522@gmail.com>
2025-03-18 01:16:07 +0700

    fixing

--- resources/views/pages/user/kontak.blade.php
+++ resources/views/pages/user/kontak.blade.php
@@ -1,21 +1,21 @@
 <x-user-layout>
     <div>
-        <!-- Background Image with Overlay -->
         <div class="pt-20 bg-black bg-opacity-50 relative overflow-hidden">
             <div class="relative w-full overflow-hidden rounded-lg h-[500px] bg-cover bg-center bg-no-repeat ">
                 <iframe class="w-full h-full border-0 entered-lazy-loaded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1979.9896250760278!2d110.40897349415624!3d-7.011723018897256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b712660f199%3A0xb346261886e0e32b!2sSekolah%20Dasar%20Negeri%20Gajahmungkur%2001!5e0!3m2!1sid!2sid!4v1742188829840!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Gallery div -->
-    <div class="py-16 bg-gray-50 scroll-mt-24" id="contact">
-        <div class="container max-w-6xl mx-auto px-4 sm:px-6">
-            <!-- div Header -->
-            <div class="text-center mb-12" data-aos="fade-up">
-                <h2 class="text-4xl py-1.5  font-bold text-gray-800 relative inline-block">
-                    Hubungi Kami
-                    <span class="absolute bottom-0 left-0 w-full h-1 gradient"></span>
-                </h2>
-                <p class="mt-4 text-lg text-gray-600 max-w-2xl mx-auto">SD N 1 Semarang</p>
             </div>
         </div>
+        <!-- Gallery div -->
+        <div class="py-16 bg-gray-50 scroll-mt-24" id="contact">
+            <div class="container max-w-6xl mx-auto px-4 sm:px-6">
+                <!-- div Header -->
+                <div class="text-center mb-12" data-aos="fade-up">
+                    <h2 class="text-4xl font-bold text-gray-800 relative inline-block">
+                        Hubungi Kami
+                        <span class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-teal-500"></span>
+                    </h2>
+                    <p class="mt-4 text-lg text-gray-600 max-w-2xl mx-auto">SD N 1 Semarang</p>
+                </div>
                 <!-- Contact Info Grid -->
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     <!-- Address Card -->
@@ -26,7 +26,6 @@
                         <h3 class="text-lg font-semibold">Alamat</h3>
                         <p class="text-gray-600 mt-2">Jl. Cumi-cumi Raya No 2, Bandarharjo, Kec. Semarang Utara, Kota Semarang, Jawa Tengah</p>
                     </div>
-
                     <!-- Contact Card -->
                     <div class="bg-white p-6 rounded-lg shadow-md text-center" data-aos="fade-up" data-aos-delay="200">
                         <div class="text-yellow-500 text-4xl mb-4">
@@ -36,7 +35,6 @@
                         <p class="text-gray-600 mt-2">sdnbandarharjo01@gmail.com</p>
                         <p class="text-gray-600">Telp: 024-3551189</p>
                     </div>
-
                     <!-- Working Hours Card -->
                     <div class="bg-white p-6 rounded-lg shadow-md text-center" data-aos="fade-up" data-aos-delay="250">
                         <div class="text-green-500 text-4xl mb-4">
@@ -53,4 +51,5 @@
             </div>
         </div>
     </div>
+
 </x-user-layout>
