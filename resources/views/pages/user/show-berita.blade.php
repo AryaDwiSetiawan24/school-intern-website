208f6c7b643a09cfed2302193a57f6c9e7be6677
Sholifudin hudaf <sholifudinhudaf@gmail.com>
2025-03-18 16:17:08 +0700

    Tampilan Berita Lihat Selengkapnya

--- resources/views/pages/user/show-berita.blade.php
+++ resources/views/pages/user/show-berita.blade.php
@@ -1,5 +1,10 @@
 <x-user-layout>
-    <div class="h-96 flex items-center justify-center">
-        halaman show berita
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">{{ $berita->judul }}</h1>
+        <p class="mb-4">{!! $berita->isi !!}</p>
+        @if($berita->gambar)
+        <img src="{{ asset('storage/' . $berita->gambar) }}" alt="Gambar Berita" class="mb-4 w-1/4">
+        @endif
+        <a href="{{ url()->previous() }}" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded">Kembali</a>
     </div>
 </x-user-layout>
