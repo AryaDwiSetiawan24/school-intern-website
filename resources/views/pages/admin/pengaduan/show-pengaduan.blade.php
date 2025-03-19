8ed012dff84a8fbf154970e6c7a5bdfd6d7738cc
Arya Dwi S <aryadwis24@gmail.com>
2025-03-19 22:57:23 +0700

    fix pengaduan

--- resources/views/pages/admin/pengaduan/show-pengaduan.blade.php
+++ resources/views/pages/admin/pengaduan/show-pengaduan.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="max-w-5xl mx-auto p-6 bg-white shadow-md rounded-lg">
-        <h2 class="text-2xl font-bold text-gray-800 mb-4 text-center">Daftar Pengaduan</h2>
+    <div class="max-w-5xl mx-auto p-6">
+        <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-4 text-center">Daftar Pengaduan</h2>
 
         <!-- Notifikasi Sukses -->
         @if(session('success'))
